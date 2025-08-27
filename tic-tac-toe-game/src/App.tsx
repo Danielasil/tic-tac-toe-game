2eced2d2453de1fccac503694eeaad8bd469d516
Daniela Silva <danielasilva081996@gmail.com>
2025-08-27 11:34:17 +0200

    Delete BrowserRouter, use HashRouter

--- tic-tac-toe-game/src/App.tsx
+++ tic-tac-toe-game/src/App.tsx
@@ -1,16 +1,13 @@
-
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage.tsx";
 import GamePage from "./GamePage.tsx";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/game" element={<GamePage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/game" element={<GamePage />} />
+    </Routes>
   );
 }
