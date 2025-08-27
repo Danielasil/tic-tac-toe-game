5b107acd4bf76012c578e099c1e1d628a520a12f
Daniela Silva <danielasilva081996@gmail.com>
2025-08-27 11:19:07 +0200

    Use HashRouter for GitHub Pages

--- tic-tac-toe-game/src/index.js
+++ tic-tac-toe-game/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App.tsx";
+import { HashRouter as Router } from 'react-router-dom'; // <- IMPORTANTE
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
-
